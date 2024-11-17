e5d386ec4b8f89f2363674e72fd0923d22802e9f
infernal <nikita.dolgopolov@gmail.com>
2024-11-17 14:51:35 +0300

    default_locale = :ru

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module BasicRails8App
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.default_locale = :ru
   end
 end
