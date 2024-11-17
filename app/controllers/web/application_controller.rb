3dc5de65f124b4afa2a65bfdbea0800a2f9bb8e2
infernal <nikita.dolgopolov@gmail.com>
2024-11-17 16:29:42 +0300

    add pundit and install

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -1,2 +1,10 @@
 class Web::ApplicationController < ApplicationController
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash.now[:alert] = 'Для выполнения данного действия необходимо авторизоваться.'
+    redirect_to(root_path)
+  end
 end
