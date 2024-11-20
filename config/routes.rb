ad165ec7ce21caa26229368e92394a4425c7eaa0
infernal <nikita.dolgopolov@gmail.com>
2024-11-20 12:49:05 +0300

    users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  scope module: :web do
-    devise_for :users
-  end
-  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get 'up' => 'rails/health#show', as: :rails_health_check
+  # get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
@@ -15,4 +11,33 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  scope module: :web do
+    #--- users ---#
+    authenticated :user do
+      root to: 'home#index', as: :authenticated_root
+    end
+    root to: redirect('/users/sign_in')
+
+    devise_for :users, controllers: {
+      sessions: 'web/users/sessions',
+      registrations: 'web/users/registrations',
+      passwords: 'web/users/passwords',
+      confirmations: 'web/users/confirmations',
+      unlocks: 'web/users/unlocks'
+    }
+
+    scope '/admin' do
+      resources :users, except: %i[show edit update destroy]
+    end
+
+    resources :users, only: %i[show edit update destroy] do
+      member do
+        post :lock
+        post :unlock
+        post :restore
+      end
+    end
+    #----#
+  end
 end
