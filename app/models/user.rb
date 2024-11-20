31f73c5786749fd9f93e5a7361e9b1d64a5d16b4
infernal <nikita.dolgopolov@gmail.com>
2024-11-20 12:59:08 +0300

    login via username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,39 @@
 class User < ApplicationRecord
-  include UserRoleEnum
+  # include UserRoleEnum
   include Discard::Model
 
   # Include default devise modules. Others available are:
-  # :timeoutable and :omniauthable
-  devise :database_authenticatable, :confirmable, :recoverable, :registerable, :trackable, :validatable,
-         :rememberable, :lockable
+  # :timeoutable
+  devise :database_authenticatable, :confirmable, :recoverable, :registerable, :trackable, :validatable, :rememberable,
+         :lockable # , :omniauthable, omniauth_providers: [:google_oauth2]
+
+  validates :username, presence: true, format: { with: /^[a-zA-Z0-9_.]*$/, multiline: true }, uniqueness: true
+  validate :validate_username
+
+  attr_writer :login
+
+  before_save do
+    username.downcase!
+  end
+
+  def login
+    @login || username || email
+  end
+
+  # Overriding the find_for_database_authentication method for login via username or email
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(['lower(username) = :value OR lower(email) = :value',
+                                    { value: login.downcase }]).first
+    elsif conditions.key?(:username) || conditions.key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+
+  private
+
+  def validate_username
+    errors.add(:username, :invalid) if User.exists?(email: username)
+  end
 end
