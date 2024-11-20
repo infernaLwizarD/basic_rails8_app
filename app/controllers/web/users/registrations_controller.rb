94e5e51c10c2be2e427e64894ddb6681e3571a21
infernal <nikita.dolgopolov@gmail.com>
2024-11-20 13:39:53 +0300

    set default role on registration

--- app/controllers/web/users/registrations_controller.rb
+++ app/controllers/web/users/registrations_controller.rb
@@ -10,9 +10,29 @@ class Web::Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  # Overriding the create method for set default role
+  def create
+    build_resource(sign_up_params)
+
+    resource.role = :user # set default role on registration
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
 
   # GET /resource/edit
   # def edit
