d108ac555aa3a3f949988a4ecd8d7cd56668e483
infernal <nikita.dolgopolov@gmail.com>
2024-12-09 01:09:16 +0300

    render methods

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -1,6 +1,18 @@
 class Web::ApplicationController < ApplicationController
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  # def render_main_turbo_stream(**)
+  #   render('layouts/lte/turbo_streams/main', **)
+  # end
+
+  def render_turbo_flash
+    turbo_stream.update('flash', partial: 'layouts/lte/partials/flash')
+  end
+
+  def render_turbo_breadcrumbs
+    turbo_stream.update('breadcrumbs', partial: 'layouts/lte/partials/breadcrumbs')
+  end
+
   private
 
   def user_not_authorized
