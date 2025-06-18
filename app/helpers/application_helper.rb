b6bc3dfa4fad2d9ea2b67592f77275bfb25bd2f6
infernal <nikita.dolgopolov@gmail.com>
2025-06-18 17:54:29 +0300

    validation with field highlighting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,21 @@ module ApplicationHelper
     options[:data][:turbo_stream] = true
     link_to(name, path, options)
   end
+
+  def field_class(object, field)
+    base_class = 'form-control form-control-sm w-500 mw-100'
+    if object.errors[field].present?
+      "#{base_class} is-invalid"
+    else
+      base_class
+    end
+  end
+
+  def error_message_for(object, field)
+    return if object.errors[field].blank?
+
+    content_tag(:div, class: 'invalid-feedback d-block') do
+      object.errors[field].join(', ')
+    end
+  end
 end
