b223ecbf1d7fa2c193caed52c3063977e0c9d988
infernal <nikita.dolgopolov@gmail.com>
2025-07-03 15:25:21 +0300

    chore: env refactoring

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,10 +25,10 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
-  config.assume_ssl = true
+  config.assume_ssl = ActiveRecord::Type::Boolean.new.cast(ENV.fetch('ASSUME_SSL', 'true'))
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = ActiveRecord::Type::Boolean.new.cast(ENV.fetch('FORCE_SSL', 'true'))
 
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
@@ -59,6 +59,7 @@ Rails.application.configure do
 
   # Set host to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: 'example.com' }
+  config.action_mailer.perform_deliveries = ActiveRecord::Type::Boolean.new.cast(ENV.fetch('PERFORM_DELIVERIES', 'true'))
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
   # config.action_mailer.smtp_settings = {
